9d5b0fd423ccbbe7d15b422ceba70b93a52cbd56
zuoshui <1594133583@qq.com>
2022-05-31 11:27:32 +0800

    new URL +view

--- lists/tests.py
+++ lists/tests.py
@@ -2,35 +2,101 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from lists.models import Item
+from lists.models import List
 
-#lists是单元测 functional_test是功能测
-# # 冒烟测试
-# class SmokeTest(TestCase):
-#
-#     def test_bad_maths(self):
-#         self.assertEqual(1+1,3)
+
+# lists是单元测 functional_test是功能测
 
 class HomePageTest(TestCase):
-
-    # def test_root_utl_resolve_to_home_page_view(self):
-    #     found = resolve('/')
-    #     self.assertEqual(found.func,home_page)
-    #
-    # def test_home_page_returns_correct_html(self):
-    #     response = self.client.get('/')
-    #
-    #     html = response.content.decode('utf8')
-    #     self.assertTrue(html.startswith('<html>'))
-    #     self.assertIn('<title>To-Do lists</title',html)
-    #     self.assertTrue(html.strip().endswith('</html>'))
-    #     self.assertTemplateUsed(response,'home.html')
-    #
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item',response.content.decode())
-        self.assertTemplateUsed(response,'home.html')
-# Create your tests here.
+    # def test_displays_all_lists_items(self):
+    #     Item.objects.create(text='itemey 1')
+    #     Item.objects.create(text='itemey 2')
+    #
+    #     response = self.client.get('/')
+    #
+    #     self.assertIn('itemey 1',response.content.decode())
+    #     self.assertIn('itemey 2', response.content.decode())
+
+
+class ListAndItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(f'/lists/{correct_list.id}/add_item',
+                         data={'item_text': 'A new item for an existing list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_after_POST(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.post(f'/lists/{correct_list.id}/add_item'
+                                    , data={'item_text': 'A new item for an existing list'})
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_display_only_item_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item1', list=other_list)
+        Item.objects.create(text='other list item2', list=other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item1')
+        self.assertNotContains(response, 'other list item2')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
